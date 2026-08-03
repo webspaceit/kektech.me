15245ec6f3407f6cc7d8eee8436f057c0649efa2
webspaceit <webspaceit@users.noreply.github.com>
2026-08-03 17:04:47 +0600

    Add bg-gray-100 to AdminLayout to override dark green body gradient

--- resources/js/components/AdminLayout.jsx
+++ resources/js/components/AdminLayout.jsx
@@ -21,7 +21,7 @@ export default function AdminLayout({ children, title }) {
     };
 
     return (
-        <div className="flex h-screen overflow-hidden">
+        <div className="flex h-screen overflow-hidden bg-gray-100 dark:bg-gray-900">
             <aside className="w-64 bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700 flex flex-col shrink-0">
                 <div className="p-4 border-b border-gray-200 dark:border-gray-700">
                     <a href="/wsdashboard" className="text-lg font-bold text-gray-900 dark:text-white">
